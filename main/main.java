9eef1dd23636b0a678fd827826b2ec0651ad9ac1
jayaprakash256006-coder <jayaprakash256006@gmail.com>
2026-03-10 22:59:59 +0530

    Initial commit

---
+++ main/main.java
@@ -0,0 +1,21 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hash Table Applications Project");
+        System.out.println("--------------------------------");
+
+        System.out.println("Problem 1: Username Availability System");
+        System.out.println("Problem 2: Flash Sale Inventory Manager");
+        System.out.println("Problem 3: DNS Cache with TTL");
+        System.out.println("Problem 4: Plagiarism Detection System");
+        System.out.println("Problem 5: Real-Time Analytics Dashboard");
+        System.out.println("Problem 6: Distributed Rate Limiter");
+        System.out.println("Problem 7: Autocomplete System");
+        System.out.println("Problem 8: Parking Lot Management");
+        System.out.println("Problem 9: Two-Sum Transaction Detection");
+        System.out.println("Problem 10: Multi-Level Cache System");
+
+        System.out.println("\nEach problem is implemented in separate branches.");
+    }
+}
